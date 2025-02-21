f1a2060baaa01e04958a391482f33d815e6baa16
Emilostuff <emilostuff@gmail.com>
2025-02-21 18:01:34 +0100

    added movement and visualisation of explored states + frontier

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import pygame
 from perlin_noise import PerlinNoise
 from algoaid import MinHeap
 from dataclasses import dataclass
+import random
 
 
 @dataclass(frozen=True)
@@ -12,8 +13,9 @@ class State:
 
 class Game:
     INSET = 10
-    THRESHOLD = 0.06
-    SEED = 16
+    THRESHOLD = 0.08
+    SEED = random.randint(0, 1000)
+    SQRT2 = 2**0.5
 
     def __init__(self, width, height):
         self.width = width
@@ -47,17 +49,25 @@ class Game:
                 if self.grid[y][x]:
                     return State(x, y)
 
+    def is_valid(self, state):
+        return (
+            0 <= state.x < self.width
+            and 0 <= state.y < self.height
+            and self.grid[state.y][state.x]
+        )
+
     def valid_moves(self, state):
         # Player can move orhoganally (cost 1) and diagonally (cost sqrt(2))
         actions = []
         for dy in [-1, 0, 1]:
             for dx in [-1, 0, 1]:
-                if 0 <= state.x + dx < self.width and 0 <= state.y + dy < self.height:
-                    if self.grid[state.y + dy][state.x + dx]:
-                        if dx == 0 or dy == 0:
-                            actions.append((State(state.x + dx, state.y + dy), 1))
-                        else:
-                            actions.append((State(state.x + dx, state.y + dy), 2**0.5))
+                new = State(state.x + dx, state.y + dy)
+                if self.is_valid(new):
+                    if dx == 0 or dy == 0:
+                        actions.append((new, 1))
+                    else:
+                        actions.append((new, Game.SQRT2))
+
         return actions
 
     def is_goal(self, state):
@@ -72,54 +82,106 @@ class Renderer:
     LIGHT = (227, 211, 191)  # Land
     DARK = (148, 133, 115)  # Mountain
     START = (214, 36, 99)  # Red
-    GOAL = (18, 166, 48)  # Green
+    GOAL = (9, 150, 18)  # Green
     PATH = (44, 168, 52)  # Path Green
-    TILE_SIZE = 4
+    EXPLORED = (227, 202, 191)
+    FRONTIER = (214, 159, 167)
+    TILE_SIZE = 8
 
     def __init__(self, game):
         self.game = game
 
-        pygame.init()
-        self.window = pygame.display.set_mode((game.width * Renderer.TILE_SIZE, game.height * Renderer.TILE_SIZE))
+        self.window = pygame.display.set_mode(
+            (game.width * Renderer.TILE_SIZE, game.height * Renderer.TILE_SIZE)
+        )
         pygame.display.set_caption("Pathfinding")
 
-    def draw(self, path=None):
+    def draw(self, path=None, astar=None):
+        # Draw map
         for y in range(self.game.height):
             for x in range(self.game.width):
                 if self.game.grid[y][x]:
                     pygame.draw.rect(
                         self.window,
                         Renderer.LIGHT,
-                        (x * Renderer.TILE_SIZE, y * Renderer.TILE_SIZE, Renderer.TILE_SIZE, Renderer.TILE_SIZE),
+                        (
+                            x * Renderer.TILE_SIZE,
+                            y * Renderer.TILE_SIZE,
+                            Renderer.TILE_SIZE,
+                            Renderer.TILE_SIZE,
+                        ),
                     )
                 else:
                     pygame.draw.rect(
                         self.window,
                         Renderer.DARK,
-                        (x * Renderer.TILE_SIZE, y * Renderer.TILE_SIZE, Renderer.TILE_SIZE, Renderer.TILE_SIZE),
+                        (
+                            x * Renderer.TILE_SIZE,
+                            y * Renderer.TILE_SIZE,
+                            Renderer.TILE_SIZE,
+                            Renderer.TILE_SIZE,
+                        ),
                     )
 
-        # Draw path 
+        # Draw explored
+        if astar:
+            for state in astar.explored:
+                pygame.draw.rect(
+                    self.window,
+                    Renderer.EXPLORED,
+                    (
+                        state.x * Renderer.TILE_SIZE,
+                        state.y * Renderer.TILE_SIZE,
+                        Renderer.TILE_SIZE,
+                        Renderer.TILE_SIZE,
+                    ),
+                )
+
+            # Draw frontier
+            for state in astar.in_frontier:
+                pygame.draw.rect(
+                    self.window,
+                    Renderer.FRONTIER,
+                    (
+                        state.x * Renderer.TILE_SIZE,
+                        state.y * Renderer.TILE_SIZE,
+                        Renderer.TILE_SIZE,
+                        Renderer.TILE_SIZE,
+                    ),
+                )
+
+        # Draw path
         if path:
             for state in path:
                 pygame.draw.rect(
                     self.window,
                     Renderer.PATH,
-                    (state.x * Renderer.TILE_SIZE, state.y * Renderer.TILE_SIZE, Renderer.TILE_SIZE, Renderer.TILE_SIZE),
+                    (
+                        state.x * Renderer.TILE_SIZE,
+                        state.y * Renderer.TILE_SIZE,
+                        Renderer.TILE_SIZE,
+                        Renderer.TILE_SIZE,
+                    ),
                 )
 
         # Draw start and goal
         pygame.draw.circle(
             self.window,
             Renderer.START,
-            (self.game.start.x * Renderer.TILE_SIZE + Renderer.TILE_SIZE / 2, self.game.start.y * Renderer.TILE_SIZE + Renderer.TILE_SIZE / 2),
-            Renderer.TILE_SIZE * 2
+            (
+                self.game.start.x * Renderer.TILE_SIZE + Renderer.TILE_SIZE / 2,
+                self.game.start.y * Renderer.TILE_SIZE + Renderer.TILE_SIZE / 2,
+            ),
+            Renderer.TILE_SIZE,
         )
         pygame.draw.circle(
             self.window,
             Renderer.GOAL,
-            (self.game.goal.x * Renderer.TILE_SIZE + Renderer.TILE_SIZE / 2, self.game.goal.y * Renderer.TILE_SIZE + Renderer.TILE_SIZE / 2),
-            Renderer.TILE_SIZE * 2
+            (
+                self.game.goal.x * Renderer.TILE_SIZE + Renderer.TILE_SIZE / 2,
+                self.game.goal.y * Renderer.TILE_SIZE + Renderer.TILE_SIZE / 2,
+            ),
+            Renderer.TILE_SIZE,
         )
 
         pygame.display.flip()
@@ -133,7 +195,7 @@ class Node:
 
 
 class AStar:
-    WEIGHT = 1
+    WEIGHT = 1.25
 
     def __init__(self, game):
         self.game = game
@@ -203,18 +265,37 @@ class AStar:
 
 
 if __name__ == "__main__":
-    game = Game(200, 200)
-    astar = AStar(game)
-    path = astar.search()
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    game = Game(100, 100)
     renderer = Renderer(game)
-    renderer.draw(path=path)
 
     # Main loop
     running = True
     while running:
+        clock.tick(30)
         # Event handling
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                # Quit pygame
-                pygame.quit()
+
+        # Handle movement
+        keys = pygame.key.get_pressed()
+        new_pos = game.start
+        if keys[pygame.K_LEFT]:
+            new_pos = State(game.start.x - 1, game.start.y)
+        if keys[pygame.K_RIGHT]:
+            new_pos = State(game.start.x + 1, game.start.y)
+        if keys[pygame.K_UP]:
+            new_pos = State(game.start.x, game.start.y - 1)
+        if keys[pygame.K_DOWN]:
+            new_pos = State(game.start.x, game.start.y + 1)
+        if game.is_valid(new_pos):
+            game.start = new_pos
+
+        astar = AStar(game)
+        path = astar.search()
+        renderer.draw(path=path, astar=astar)
+
+    pygame.quit()
